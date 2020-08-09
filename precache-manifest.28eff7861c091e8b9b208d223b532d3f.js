1989f198d2259b21bfc437138305ea0dbe97a909
warbonnet333 <warbonnet333@gmail.com>
2020-08-09 11:39:44 +0300

    Updates

---
+++ precache-manifest.28eff7861c091e8b9b208d223b532d3f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86eb096b158a7404e5bf3513a4314254",
+    "url": "/front/index.html"
+  },
+  {
+    "revision": "8a4a65a7900e027d56f1",
+    "url": "/front/static/css/main.fe6b82ad.chunk.css"
+  },
+  {
+    "revision": "c2c85b8872d914c2c953",
+    "url": "/front/static/js/2.edb9cd81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/front/static/js/2.edb9cd81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a4a65a7900e027d56f1",
+    "url": "/front/static/js/main.f0be427e.chunk.js"
+  },
+  {
+    "revision": "8aff28e2b3aa97bf6587",
+    "url": "/front/static/js/runtime-main.2d2c1fda.js"
+  },
+  {
+    "revision": "f3f9cff29a6db95ba6a9f1aa4bb0da4e",
+    "url": "/front/static/media/536270-PJIICW-380 1.f3f9cff2.png"
+  },
+  {
+    "revision": "ea8692911735ae37834e5ee3c7be8024",
+    "url": "/front/static/media/Ellipse 45.ea869291.svg"
+  },
+  {
+    "revision": "f1801a21e6d98c386588a23861d3b212",
+    "url": "/front/static/media/Neck_POP_40yr_Old_Asian_Female_02_075_1536x960 1.f1801a21.png"
+  },
+  {
+    "revision": "21b6448de11dcc5954b521825f915448",
+    "url": "/front/static/media/Vector.21b6448d.svg"
+  },
+  {
+    "revision": "db26fc63c0df81654f40618343065d71",
+    "url": "/front/static/media/_.db26fc63.svg"
+  },
+  {
+    "revision": "07486179ee4f10370d52ff7bbbdf5f24",
+    "url": "/front/static/media/image 10.07486179.svg"
+  },
+  {
+    "revision": "5fba9542fe413cbc9d7665535a1f2691",
+    "url": "/front/static/media/image 11.5fba9542.svg"
+  },
+  {
+    "revision": "00bfe2ab3dada51dbe8afba1d96bc3bf",
+    "url": "/front/static/media/image 13.00bfe2ab.svg"
+  },
+  {
+    "revision": "4d0ec0ea44a524d89aa6143a0041688b",
+    "url": "/front/static/media/image 18.4d0ec0ea.svg"
+  },
+  {
+    "revision": "fa7ada40b663cd21e251b9a7e0f9bf9a",
+    "url": "/front/static/media/image 9.fa7ada40.svg"
+  },
+  {
+    "revision": "dafbb86c74fd86e5b8f2b0d5d0902cd9",
+    "url": "/front/static/media/long.dafbb86c.png"
+  },
+  {
+    "revision": "27c0d9280d3e531ff42747e7f757b4dd",
+    "url": "/front/static/media/vorte_long.27c0d928.png"
+  }
+]);
